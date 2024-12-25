f00ecb8445443f2156d9c1100c2422467b096bd3
AnTran2806 <duyan20191999@gmail.com>
2024-12-25 20:57:28 +0700

    Tái cấu trúc và tối ưu code #1

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import Header from '~/components/Layout/components/Header';
+import Header from '~/layouts/components/Header';
 import styples from './DefaultLayout.module.scss';
 import Sidebar from './Sidebar';
 
